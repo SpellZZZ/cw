71ae44a63f0b620fcc0734373a729e0d3a3b898b
SpellZZZ <james321321@wp.pl>
2024-02-27 19:31:33 +0100

    Branch init 2

---
+++ src/main/java/org/example/model/Employee.java
@@ -0,0 +1,16 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+
+}
