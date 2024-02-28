525284c1eff7ee8f266d511ecb133f890a12057b
SpellZZZ <james321321@wp.pl>
2024-02-28 11:52:09 +0100

    ADD repo's methods

--- src/main/java/org/example/dao/EmployeeRepo.java
+++ src/main/java/org/example/dao/EmployeeRepo.java
@@ -4,5 +4,6 @@ import org.example.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepo extends JpaRepository <Employee, Long> {
-
+    Employee findByName(String name);
+    Employee findBySurname(String surname);
 }
--- src/main/java/org/example/dao/EmployerRepo.java
+++ src/main/java/org/example/dao/EmployerRepo.java
@@ -1,8 +1,10 @@
 package org.example.dao;
 
+import org.example.model.Employee;
 import org.example.model.Employer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployerRepo extends JpaRepository<Employer, Long> {
-
+    Employee findByName(String name);
+    Employee findBySurname(String surname);
 }
