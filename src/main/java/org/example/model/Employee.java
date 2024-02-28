9af662f288725ee8cd54ccf45757dec49de2fd32
SpellZZZ <james321321@wp.pl>
2024-02-28 13:08:19 +0100

    ADD getters and setters in model

--- src/main/java/org/example/model/Employee.java
+++ src/main/java/org/example/model/Employee.java
@@ -22,4 +22,20 @@ public class Employee {
         this.name = name;
         this.surName = surName;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
 }
